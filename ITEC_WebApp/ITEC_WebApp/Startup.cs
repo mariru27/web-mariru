267788562f7004af83cb7baac1b97f0f5e453f75
mariru27 <marina.rusu.99@gmail.com>
2021-03-27 15:53:44 +0200

    mapControllerRoute change

--- ITEC_WebApp/ITEC_WebApp/Startup.cs
+++ ITEC_WebApp/ITEC_WebApp/Startup.cs
@@ -50,7 +50,7 @@ namespace ITEC_WebApp
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Countries}/{id?}");
             });
         }
     }
