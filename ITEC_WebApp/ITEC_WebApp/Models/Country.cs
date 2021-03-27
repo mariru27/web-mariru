17ccff1f7e3c5355de537a42d086aafd271f9b2a
mariru27 <marina.rusu.99@gmail.com>
2021-03-27 13:16:42 +0200

    Added country model

---
+++ ITEC_WebApp/ITEC_WebApp/Models/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITEC_WebApp.Models
+{
+    public class Country
+    {
+        [Key]
+        public int IdCountry { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+
+
+    }
+}
--- ITEC_WebApp/ITEC_WebApp/Models/User.cs
+++ ITEC_WebApp/ITEC_WebApp/Models/User.cs
@@ -1,7 +1,10 @@
-﻿namespace ITEC_WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITEC_WebApp.Models
 {
     public class User
     {
+        [Key]
         public int IdUser { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
