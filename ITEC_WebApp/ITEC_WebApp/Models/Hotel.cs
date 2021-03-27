dbfeb15f38c6fa040c97107d7aad459613465b10
mariru27 <marina.rusu.99@gmail.com>
2021-03-27 13:21:25 +0200

    Add hotel model

---
+++ ITEC_WebApp/ITEC_WebApp/Models/Hotel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITEC_WebApp.Models
+{
+    public class Hotel
+    {
+        [Key]
+        public int IdHotel { get; set; }
+        public string Name { get; set; }
+        public int Stars { get; set; }
+        public string Description { get; set; }
+    }
+}
