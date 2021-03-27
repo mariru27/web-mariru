7663f4e702dacf9470e87d1a5cb1453418807f3c
mariru27 <marina.rusu.99@gmail.com>
2021-03-27 13:12:38 +0200

    Added User Model

---
+++ ITEC_WebApp/ITEC_WebApp/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace ITEC_WebApp.Models
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+    }
+}
