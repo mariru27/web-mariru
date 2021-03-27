5d23b803471b0eb098d314cfbf8a9487331343c1
mariru27 <marina.rusu.99@gmail.com>
2021-03-27 23:15:23 +0200

    Create searchModel

---
+++ ITEC_WebApp/ITEC_WebApp/ViewModel/SearchModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ITEC_WebApp.ViewModel
+{
+    public class SearchModel
+    {
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Hotel { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+    }
+}
