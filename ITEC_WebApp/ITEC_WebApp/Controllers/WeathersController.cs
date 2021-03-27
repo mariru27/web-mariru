4b0103a3e86b3693ad26e87adae1e577370f72a0
mariru27 <marina.rusu.99@gmail.com>
2021-03-27 14:41:19 +0200

    CRUD Weathers

---
+++ ITEC_WebApp/ITEC_WebApp/Controllers/WeathersController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ITEC_WebApp.Data;
+using ITEC_WebApp.Models;
+
+namespace ITEC_WebApp.Controllers
+{
+    public class WeathersController : Controller
+    {
+        private readonly ContextITEC _context;
+
+        public WeathersController(ContextITEC context)
+        {
+            _context = context;
+        }
+
+        // GET: Weathers
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Weather.ToListAsync());
+        }
+
+        // GET: Weathers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var weather = await _context.Weather
+                .FirstOrDefaultAsync(m => m.IdWheather == id);
+            if (weather == null)
+            {
+                return NotFound();
+            }
+
+            return View(weather);
+        }
+
+        // GET: Weathers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Weathers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdWheather,Details")] Weather weather)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(weather);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(weather);
+        }
+
+        // GET: Weathers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var weather = await _context.Weather.FindAsync(id);
+            if (weather == null)
+            {
+                return NotFound();
+            }
+            return View(weather);
+        }
+
+        // POST: Weathers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdWheather,Details")] Weather weather)
+        {
+            if (id != weather.IdWheather)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(weather);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!WeatherExists(weather.IdWheather))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(weather);
+        }
+
+        // GET: Weathers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var weather = await _context.Weather
+                .FirstOrDefaultAsync(m => m.IdWheather == id);
+            if (weather == null)
+            {
+                return NotFound();
+            }
+
+            return View(weather);
+        }
+
+        // POST: Weathers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var weather = await _context.Weather.FindAsync(id);
+            _context.Weather.Remove(weather);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool WeatherExists(int id)
+        {
+            return _context.Weather.Any(e => e.IdWheather == id);
+        }
+    }
+}
