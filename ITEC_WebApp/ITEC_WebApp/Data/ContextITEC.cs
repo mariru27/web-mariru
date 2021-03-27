16317ed90d0538fc0cd649df1f15357cc3dded20
mariru27 <marina.rusu.99@gmail.com>
2021-03-27 14:09:01 +0200

    Add in context all DbSets

--- ITEC_WebApp/ITEC_WebApp/Data/ContextITEC.cs
+++ ITEC_WebApp/ITEC_WebApp/Data/ContextITEC.cs
@@ -11,10 +11,25 @@ namespace ITEC_WebApp.Data
 
         }
         public DbSet<Test> Test { get; set; }
+        public DbSet<City> City { get; set; }
+        public DbSet<Country> Country { get; set; }
+        public DbSet<Covid> Covid { get; set; }
+        public DbSet<Hotel> Hotel { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<Room> Room { get; set; }
+        public DbSet<User> User { get; set; }
+        public DbSet<Weather> Weather { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Test>().ToTable("Test");
+            modelBuilder.Entity<Test>().ToTable("City");
+            modelBuilder.Entity<Test>().ToTable("Country");
+            modelBuilder.Entity<Test>().ToTable("Covid");
+            modelBuilder.Entity<Test>().ToTable("Hotel");
+            modelBuilder.Entity<Test>().ToTable("Room");
+            modelBuilder.Entity<Test>().ToTable("User");
+            modelBuilder.Entity<Test>().ToTable("Weather");
         }
     }
 }
