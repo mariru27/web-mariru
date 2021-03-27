b50f739d9eaebd6494eb14133cda4cabd16b2581
mariru27 <marina.rusu.99@gmail.com>
2021-03-27 13:36:47 +0200

    Role model

---
+++ ITEC_WebApp/ITEC_WebApp/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ITEC_WebApp.Models
+{
+    public class Role
+    {
+        [Key]
+        public int IdRole { get; set; }
+        public string NameRole { get; set; }
+    }
+}
