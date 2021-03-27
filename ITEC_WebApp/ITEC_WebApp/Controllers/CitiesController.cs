c9d33723a32678d2917f43cf562059eebfc47334
mariru27 <marina.rusu.99@gmail.com>
2021-03-27 14:42:48 +0200

    CRUD Cities

---
+++ ITEC_WebApp/ITEC_WebApp/Controllers/CitiesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ITEC_WebApp.Data;
+using ITEC_WebApp.Models;
+
+namespace ITEC_WebApp.Controllers
+{
+    public class CitiesController : Controller
+    {
+        private readonly ContextITEC _context;
+
+        public CitiesController(ContextITEC context)
+        {
+            _context = context;
+        }
+
+        // GET: Cities
+        public async Task<IActionResult> Index()
+        {
+            var contextITEC = _context.City.Include(c => c.Weather);
+            return View(await contextITEC.ToListAsync());
+        }
+
+        // GET: Cities/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _context.City
+                .Include(c => c.Weather)
+                .FirstOrDefaultAsync(m => m.IdCity == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return View(city);
+        }
+
+        // GET: Cities/Create
+        public IActionResult Create()
+        {
+            ViewData["IdCity"] = new SelectList(_context.Weather, "IdWheather", "Details");
+            return View();
+        }
+
+        // POST: Cities/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdCity,Name")] City city)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(city);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCity"] = new SelectList(_context.Weather, "IdWheather", "Details", city.IdCity);
+            return View(city);
+        }
+
+        // GET: Cities/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _context.City.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdCity"] = new SelectList(_context.Weather, "IdWheather", "Details", city.IdCity);
+            return View(city);
+        }
+
+        // POST: Cities/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdCity,Name")] City city)
+        {
+            if (id != city.IdCity)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(city);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CityExists(city.IdCity))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdCity"] = new SelectList(_context.Weather, "IdWheather", "Details", city.IdCity);
+            return View(city);
+        }
+
+        // GET: Cities/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _context.City
+                .Include(c => c.Weather)
+                .FirstOrDefaultAsync(m => m.IdCity == id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return View(city);
+        }
+
+        // POST: Cities/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var city = await _context.City.FindAsync(id);
+            _context.City.Remove(city);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CityExists(int id)
+        {
+            return _context.City.Any(e => e.IdCity == id);
+        }
+    }
+}
