acc86fc13a48ac1df477f31d5b2279c773a7c821
mariru27 <marina.rusu.99@gmail.com>
2021-03-27 13:51:50 +0200

    City model

---
+++ ITEC_WebApp/ITEC_WebApp/Models/City.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ITEC_WebApp.Models
+{
+    public class City
+    {
+        [Key]
+        public int IdCity { get; set; }
+        public string Name { get; set; }
+        public Weather Weather { get; set; }
+        public List<Hotel> Hotels { get; set; }
+
+    }
+}
