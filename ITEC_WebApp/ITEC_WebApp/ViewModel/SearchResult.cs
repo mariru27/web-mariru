ec5babc38a206c55ca605a482d9a06bdcc192b37
mariru27 <marina.rusu.99@gmail.com>
2021-03-28 01:43:42 +0200

    delete country, city, hotel filter

--- ITEC_WebApp/ITEC_WebApp/ViewModel/SearchResult.cs
+++ ITEC_WebApp/ITEC_WebApp/ViewModel/SearchResult.cs
@@ -7,8 +7,6 @@ namespace ITEC_WebApp.ViewModel
         public Country Country { get; set; }
         public City City { get; set; }
         public Hotel Hotel { get; set; }
-        public Weather Weather { get; set; }
-        public Covid Covid { get; set; }
         public Room Room { get; set; }
     }
 }
