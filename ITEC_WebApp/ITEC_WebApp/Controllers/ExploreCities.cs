40b8f39ce0555217b5b9a3f7ff65df6231d304f3
mariru27 <marina.rusu.99@gmail.com>
2021-03-27 16:19:44 +0200

    FindHotel action in exloreCities

---
+++ ITEC_WebApp/ITEC_WebApp/Controllers/ExploreCities.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ITEC_WebApp.Controllers
+{
+    public class ExploreCities : Controller
+    {
+        public IActionResult FindHotel()
+        {
+            return View();
+        }
+    }
+}
